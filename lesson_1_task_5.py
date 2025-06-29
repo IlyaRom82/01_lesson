c2f071b98ab55c12409a460a4b7a1aebd8ad9e3f
IlyaRom82 <light108@tutamail.com>
2025-06-29 22:17:49 +0300

    Явно вызвал функцию print_num 11 раз для вывода номера 88005553535

--- lesson_1_task_5.py
+++ lesson_1_task_5.py
@@ -1,6 +1,15 @@
 def print_num(num):
     print(num)
 
+print_num(88005553535)
+print_num(88005553535)
+print_num(88005553535)
+print_num(88005553535)
+print_num(88005553535)
+print_num(88005553535)
+print_num(88005553535)
+print_num(88005553535)
+print_num(88005553535)
+print_num(88005553535)
+print_num(88005553535)
 
-for _ in range(11):
-    print_num(88005553535)
